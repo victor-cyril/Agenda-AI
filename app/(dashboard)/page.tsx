069458bbb2fbcebf41626b77ea79cc2abbf19873
Victor Cyril <viicyyl@gmail.com>
2025-12-09 08:33:17 +0100

    Removed home view

--- app/(dashboard)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { validateSession } from "@/lib/server-utils";
-import HomeView from "@/modules/home/ui/views/home-view";
-import { caller, prefetch, trpc } from "@/trpc/server";
-
-export default async function Home() {
-  const session = await validateSession();
-  return <HomeView />;
-}
